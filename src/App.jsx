fd00f62b1f207c229eae0d5444ada4e1ac18d843
risqiromadhoni <risqi.romadhoni96@gmail.com>
2024-06-09 21:47:38 +0700

    feat: form input file single and multiple in formdata

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,51 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [file, setFile] = useState()
+  const [files, setFiles] = useState([])
+  const [message, setMessage] = useState("")
+
+  const handleFileChange = (e) => {
+    // type: https://developer.mozilla.org/en-US/docs/Web/API/FileList
+    const { files } = e.target
+    setFile(files.item(0))
+  }
+
+  const handleMultipleFileChange = (e) => {
+    // type: https://developer.mozilla.org/en-US/docs/Web/API/FileList
+    const { files } = e.target
+    const arrFiles = Array.from(files)
+    console.log(arrFiles)
+    setFiles(arrFiles)
+  }
+
+  const handleMessageChange = (e) => {
+    const { value } = e.target
+    setMessage(value)
+  }
+
+  const uploadData = (e) => {
+    const formData = new FormData()
+    if (!!file) formData.append("file", file)
+    if (!!message) formData.append("message", message)
+    if (!!files.length) {
+      for (const multiFile of files) {
+        console.log(multiFile)
+        formData.append("mutipleFile[]", multiFile)
+      }
+    }
+
+    console.log(Array.from(formData.entries()))
+    
+    // If you using axios to comunicate with API,
+    // you should overide default header config
+    //axios.defaults.headers.common = {
+    //  "Content-Type": "multipart/form-data",
+    //  Accept: "*/*",
+    //};
+
+    // Do upload with axios
+  }
 
   return (
     <>
@@ -28,6 +73,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="form-file">
+        <input type="file" id="newFile" onChange={handleFileChange} />
+        <input type="file" id="multiFile" multiple onChange={handleMultipleFileChange} />
+        <input type="text" id="newMessage" placeholder="An message iiinput here" onChange={handleMessageChange} />
+        <button onClick={uploadData}>Upload</button>
+      </div>
     </>
   )
 }
